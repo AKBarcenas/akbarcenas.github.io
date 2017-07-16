8992a47879e9b523e3e127f8de407740fff251de
AKBarcenas <akbarcen@ucsd.edu>
2017-07-15 19:38:32 -0700

    Change site

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import Interactive from 'react-interactive';
-import { Switch, Route } from 'react-router-dom';
-import Home from './Home';
-import ExampleComponent from './ExampleComponent';
-import PageNotFound from './PageNotFound';
-import Breadcrumbs from './Breadcrumbs';
 import s from '../styles/app.style';
 
 var FontAwesome = require('react-fontawesome');
@@ -13,9 +7,9 @@ export default function App() {
   return (
     <div style={s.App}>
       <div style={s.Appheader}>
-        <h1>Alexander Barcenas</h1>
-        <h3>I am a third year at UCSD and plan on finishing my B.S. in computer scince in June 2018.</h3>
-        <h3>I'm currently rebulding my website using React, but you can check out my resume and profiles below.</h3>
+        <h2>Alexander Barcenas</h2>
+        <h3>I am a third year at UCSD and plan on finishing my B. S. in computer science in June 2018.</h3>
+        <h3>I am currently rebulding my website using React, but you can check out my resume and profiles below.</h3>
       </div>
       <div style={s.Links}>
         <a href="AlexanderBarcenasResume.pdf" style={s.button}>Resume</a>
